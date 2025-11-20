a0baca3fbcd3e6c987607ccbf891d3fa8d9506e6
udiqhana <diqhaudiqhana@gmail.com>
2025-11-20 15:52:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 let particles = [];
-const particleCount = 10000;
+const particleCount = 100;
 
 class Particle {
     constructor() {
